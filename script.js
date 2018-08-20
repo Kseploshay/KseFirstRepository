17105b9f8881510e3231be845e26823a0fb018ed
Ksenia Ploshay <ksenia.ploshay@gmail.com>
2018-08-20 16:13:57 +0300

    Task7

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-var x = prompt ("чему равен x?");
-var y = prompt ("чему равен y?");
-var x = parseInt (x, 10);
-var y = parseInt (y, 10);
-var SUM = alert ("x + y =" + (x+y));
-
+function checkNameEmailValue(event){
+  event.preventDefault(); 
+  var name = document.getElementById("name");
+  var nameValue = name.value;
+  var form = document.getElementById("form")
+   if (nameValue === ''){
+    name.style.border = '1px solid red';
+   } else {
+    name.style.border = '1px solid black';
+  }
+   var email = document.getElementById("email");
+   var emailValue = email.value;
+   if (emailValue === ''){
+    email.style.border = '1px solid red';
+  } else {
+    email.style.border = '1px solid black';
+  }
+  if ((nameValue != "") && (emailValue != "")){form.style.display = "none",alert ("спасибо, ваша заявка принята")};
+ }
+ var button = document.getElementById("send");
+button.addEventListener("click", checkNameEmailValue);
 
 
 
